d843772e50c1b2662ca196755c7de620964fe635
dev amila <vdalakmal@gmail.com>
2024-11-30 12:32:36 +0530

    type sript fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,26 @@
 import { Module } from '@nestjs/common';
 import { JokesModule } from './jokes/jokes.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Joke } from './jokes/joke.entity';
+import { JokeType } from './joke-types/joke-type.entity';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [JokesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [Joke, JokeType],
+      synchronize: false,
+      logging: true,
+    }),
+    JokesModule,
+  ],
 })
 export class AppModule {}
